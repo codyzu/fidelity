670d8728e602abf0360f1472271f90ebd0348e3a
Cody Zuschlag <cody.zuschlag@gmail.com>
2022-11-27 16:29:20 +0100

    function to add user info when user added

---
+++ functions/src/index.ts
@@ -0,0 +1,29 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+export const addUser = functions
+  .region('europe-west1')
+  .auth.user()
+  .onCreate(async (user, context) => {
+    const userDoc: {phoneNumber?: string; email?: string} = {};
+
+    if (user.phoneNumber) {
+      userDoc.phoneNumber = user.phoneNumber;
+    }
+
+    if (user.email) {
+      userDoc.email = user.email;
+    }
+
+    console.log('USER', userDoc);
+    await admin.firestore().collection('users').add(userDoc);
+  });
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
